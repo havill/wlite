cd81c098f7d82010733ffd406a8e50e210621346
Eido Inoue <eido.a.inoue@rakuten.com>
2022-05-18 15:56:58 +0900

    Update the Name, Copyright, and License to Gen Z

--- test-wcwidth.c
+++ test-wcwidth.c
@@ -1,3 +1,21 @@
+/*
+ * wlite: A 16-bit Unicode based tiny <wchar.h> library with CJK features
+ * Copyright (C) 2022 Eido INOUE
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Lesser Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <errno.h>
 #include <stdio.h>
 #include <locale.h>
--- wlite_wmemset.c
+++ wlite_wmemset.c
@@ -1,26 +1,20 @@
 /*
- * $Id: wlite_wmemset.c,v 1.1 2002/12/10 21:57:58 havill Exp havill $
- *
- * Copyright (C) 2003  Red Hat, Inc.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Original Author: Adrian Havill <havill@redhat.com>
- *
- * Contributors:
- */
+ * wlite: A 16-bit Unicode based tiny <wchar.h> library with CJK features
+ * Copyright (C) 2022 Eido INOUE
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Lesser Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "wlite_config.h"   // wchar_t, NULL, size_t
 
