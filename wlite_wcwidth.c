b20b2370b336c5ee6675caca82a0c1066e3dc7a8
Eido Inoue <eido.a.inoue@rakuten.com>
2022-05-30 14:05:29 +0900

    fix wcwidth() for ctrl chars, especially NL

--- wlite_wcwidth.c
+++ wlite_wcwidth.c
@@ -27,6 +27,10 @@ extern const wlite_bitarray_t_ wlite_ambi[];
 
 int
 wlite_wcwidth(wchar_t c) {
+    const wchar_t DEL = (wchar_t) 0x7F; // ASCII/Latin-1
+    const wchar_t SUB = (wchar_t) 0x1A; // ASCII/Latin-1
+    const wchar_t ESC = L'\e';          // GNU extension: ASCII 1B
+
     size_t index = c / WLITE_BITARRAY_N_;
     int bit = c % WLITE_BITARRAY_N_, is_wide = 0, width = -1;
 #if WLITE_AMBI_LOCALE
@@ -105,37 +109,34 @@ wlite_wcwidth(wchar_t c) {
     //*************************************************************************
 
     default:
-        if (wlite_iswctype((c), wlite_cntrl_)) {
-            if (c == 0x08 || c == 0x7F) {  // U+0008 = \b = BS (Backspace; ^H)
-                /* XXX: wcwidth is not capable of return negative widths
-                 * because -1 is reserved for "unknown"
-                 */
-                width = -1;
-                break;
-            }
-            else if (c == 0x09) {          // U+0009 = \t = HT (Tab; ^I)
-                /* XXX: TABs are usually 1 to 8 spaces, but we have no context
-                 * as to where the cursor is.
-                 */
-
-                width = -1;
-                break;
-            }
-            else if (c == 0x1A) {          // U+001A = SUB (^Z)
-                /* FIXME: handle this like U+FFFD or U+FFFC? Unicode hints that
-                 * it should be treated like U+FFFD, but it IS a control
-                 * character, implying that an action occurs and it's not mapped
-                 * to any one glyph, similar to U+FFFC.
-                 *
-                 * BUT, In real life, Ctrl-Z is usually used like Unix's "stdin
-                 * eof" Ctrl-D-- that is, a "stdin eof" for MS-DOS. It can be
-                 * found at the end of older DOS text files, so treat it as
-                 * width 0.
-                 */
-
-                width = -1;
-                break;
-            }
+        if (c == L'\a' || c == L'\b' || c == L'\f' || c == L'\n' || c == L'\r' || c == L'\t' || c == L'\v' ||
+            c == ESC || c == DEL) {
+            /* XXX: wcwidth is not capable of return negative widths (ex. backspace)
+             * because -1 is reserved for "unknown"
+             * 
+             * also, all "cursor movement" control characters have ambiguous width depending on the terminal
+             * FIXME: ESC will probably be followed by a sequence to move the cursor, so don't return 0 width
+             */
+            width = -1;
+            break;
+        }
+        else if (c == SUB) {          // U+001A = SUB (Control-Z)
+            /* FIXME: handle this like U+FFFD or U+FFFC? Unicode hints that
+             * it should be treated like U+FFFD, but it IS a control
+             * character, implying that an action occurs and it's not mapped
+             * to any one glyph, similar to U+FFFC.
+             *
+             * BUT, In real life, Ctrl-Z is usually used like Unix's "stdin
+             * eof" Ctrl-D-- that is, a "stdin eof" for MS-DOS. It can be
+             * found at the end of older DOS text files, so treat it as
+             * width 0.
+             */
+            width = -1;
+            break;
+        }
+        else if (wlite_iswctype((c), wlite_cntrl_)) {
+            width = 0;
+            break;
         }
         width = is_wide ? 2 : 1;
         break;
